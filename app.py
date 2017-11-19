76577a2ad5814e9e23cde275a009622aab04dc87
Gunalan <gunalan.s@outlook.com>
2017-11-19 19:01:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,27 +11,10 @@ from flask import make_response
 app = Flask(__name__)
 
 
-@app.route('/webhook', methods=['GET'])
-def webhook():
-    req = request.get_json(silent=True, force=True)
 
-    print("Request:")
-    print(json.dumps(req, indent=4))
-
-    res = makeWebhookResult(req)
-
-    res = json.dumps(res, indent=4)
-    print(res)
-    r = make_response(res)
-    r.headers['Content-Type'] = 'application/json'
-    return r
-
-def makeWebhookResult(req):
    
     speech = "Hello this is my first webhook response"
 
-    print("Response:")
-    print(speech)
 
     return {
         "speech": speech,
